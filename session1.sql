b209fef3dab6f1421364248e6590aee9b5c9f63f
fatih <fdemiir1982@gmail.com>
2021-12-09 00:20:28 +0300

    first commit

---
+++ session1.sql
@@ -0,0 +1,38 @@
+/*=================================================================
+							SELECT
+===================================================================*/
+   /*track tablosundaki nameyi sorgulayın*/
+SELECT name FROM tracks;
+/*track tablosundaki composer ve nameyi sorgulayın*/
+SELECT Composer,name FROM tracks;
+
+/*track tablosundaki tüm bilgileri sorgulayın*/
+SELECT * FROM tracks;
+
+
+
+/*=================================================================
+							DISTINCT
+===================================================================*/
+/*track tablosundaki Composer bilgileri sorgulayın(tekrarlı olabilir)*/
+SELECT Composer FROM tracks;
+
+/*track tablosundaki Composer bilgileri sorgulayın(tekrarsız)*/
+SELECT DISTINCT Composer FROM tracks;
+/*track tablosundaki AlbumId, MediaTypeId bilgilerini tekrarsız olarak sorgulayın*/
+SELECT DISTINCT AlbumId, MediaTypeId FROM tracks;
+/*=================================================================
+							WHERE
+===================================================================*/
+/*Composer'ı Jimi Hendrix olan şarkıları sorgulayın*/
+
+SELECT name 
+FROM tracks 
+WHERE Composer='Jimi Hendrix';
+
+
+/*invoice tablosundaki total eğeri 10 dolardan büyük olan fatıraları tüm bilgileriComposer'ı Jimi Hendrix olan şarkıları sorgulayın*/
+SELECT * 
+FROM invoices 
+WHERE total>10;
+
